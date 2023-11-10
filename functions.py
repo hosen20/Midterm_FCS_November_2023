80a48647501d1a5c7d75e382c9e1376a1922410c
hosen20 <h.shiri@st.ul.edu.lb>
2023-11-10 21:18:15 +0200

    added all functions to the .py file

--- functions.py
+++ functions.py
@@ -1,9 +1,3 @@
-import validators
-import requests
-from bs4 import BeautifulSoup
-import time
-
-
 def validateURL():
     user_url = input("please enter url: ")
     while (not validators.url(user_url)):
@@ -11,12 +5,21 @@ def validateURL():
         user_url = input("please enter url: ")
     return user_url
 
+
+def get_index(tabs):
+    index = eval(input("enter index: "))
+    print("\n")
+    while (index >= len(tabs) or index < 0):
+        index = eval(input("enter index: "))
+        print("\n")
+    return index
+
+
 def add_tab(tabs):
     user_url = validateURL()
     title = input("please input title: ")
     print("\n")
     tabs.append({title:user_url, 'nested_tabs':[]})
-
 
 
 def get_html(url):
@@ -40,7 +43,6 @@ def get_html(url):
     return "error"
 
 
-
 def printMenu():
     print('''
     1. Open Tab
@@ -56,22 +58,48 @@ def printMenu():
 
 
 def add_nested_tab(tabs):
-    index = eval(input("enter index: "))
-    print("\n")
-    while (index >= len(tabs) or index < 0):
-        index = eval(input("enter index: "))
-        print("\n")
+    index = get_index(tabs)
     title = input("enter title: ")
     print("\n")
     url = validateURL()
     tabs[index]['nested_tabs'].append({title:url})
 
 
-
 def remove_tab(tabs):
-    index = eval(input("enter index: "))
-    print("\n")
-    while (index >= len(tabs) or index < 0):
-        index = eval(input("enter index: "))
-        print("\n")
+    index = get_index(tabs)
     tabs.pop(index)
+
+
+def display(tabs):
+    index = get_index(tabs)
+    dct = tabs[index]
+    print(get_html(dct[first_key(dct)]))
+    if len(dct['nested_tabs']) > 0:
+        for n_dct in dct['nested_tabs']:
+            print(get_html(n_dct[first_key(n_dct)]))
+
+
+def first_key(dct):
+    keys = dct.keys()
+    keys = list(keys)
+    return keys[0]
+
+
+def clear(tabs):
+    tabs = []
+
+
+def saveTabs(tabs):
+    path = input("please enter file name")
+    with open(path, "w", encoding="utf-8") as json_file:
+        json.dump(tabs, json_file, ensure_ascii=False, indent=4)
+
+
+def loadTabs():
+    path = input("enter path: ")
+    print("\n")
+    with open(path) as f:
+        data = json.load(f)
+    return data
+
+
