352d9be7dc32fd72b0eab1ca254a1d050cd8006e
hosen20 <h.shiri@st.ul.edu.lb>
2023-11-10 18:24:01 +0200

    added a function to print the menu

--- functions.py
+++ functions.py
@@ -38,3 +38,20 @@ def get_html(url):
         except:
             error = True
     return "error"
+
+
+
+def printMenu():
+    print('''
+    1. Open Tab
+    2. Close Tab
+    3. Switch Tab
+    4. Display All Tabs
+    5. Open Nested Tab
+    6. Clear All Tabs
+    7. Save Tabs
+    8. Import Tabs
+    9. Exit
+    ''')
+
+
