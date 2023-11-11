b8e01c0bf2699ec36e7d7893a8c258f8ca367f09
hosen20 <h.shiri@st.ul.edu.lb>
2023-11-11 14:58:29 +0200

    organized the file functions.py and added comments

--- functions.py
+++ functions.py
@@ -7,6 +7,98 @@ import json
 
 
 
+# function used in choice 1
+def add_tab(tabs):
+    user_url = validate_url()
+    title = add_title(tabs)
+    tabs.append({title:user_url, 'nested_tabs':[]})
+
+# function used in choice 2
+def remove_tab(tabs):
+    if len(tabs) > 0:
+        index = -2
+        while (index >= len(tabs) or index < -1):
+            index = get_index(tabs)
+        tabs.pop(index)
+    else:
+        print("nothing to remove, no tabs opened\n")
+
+
+# function used in choice 3
+def display(tabs):
+    if len(tabs) == 0:
+        print("nothing to display\n")
+    else:
+        index = -2
+        while (index >= len(tabs) or index < -1):
+            index = get_index(tabs)
+        dct = tabs[index]
+        print(get_html(dct[first_key(dct)]))
+        if len(dct['nested_tabs']) > 0:
+            for n_dct in dct['nested_tabs']:
+                print(get_html(n_dct[first_key(n_dct)]))
+
+
+# function used in choice 4
+def display_titles(tabs):
+    if len(tabs) == 0:
+        print("no opened tabs")
+    else:
+        for index in range(len(tabs)):
+            dct = tabs[index]
+            print(first_key(dct))
+            if len(dct['nested_tabs']) > 0:
+                for n_dct in dct['nested_tabs']:
+                    print(f'\t--{first_key(n_dct)}')
+
+
+# function used in choice 5
+def add_nested_tab(tabs):
+    if len(tabs) == 0:
+        print("no opened tabs")
+    else:
+        index = -2
+        while (index >= len(tabs) or index < -1):
+            index = get_index(tabs)
+        title = add_title(tabs[index]['nested_tabs'])
+        print("\n")
+        url = validate_url()
+        tabs[index]['nested_tabs'].append({title:url})
+
+
+# function used in choice 6
+def clear(tabs):
+    tabs = []
+
+
+# function used in choice 7
+def save_tabs(tabs):
+    path = ''
+    while ('json' not in path):
+        path = input("enter path: ")
+        print("\n")
+    with open(path, "w", encoding="utf-8") as json_file:
+        json.dump(tabs, json_file, ensure_ascii=False, indent=4)
+
+
+# function used for choice 8
+def load_tabs(tabs):
+    path = ''
+    while ('.json' not in path):
+        path = input("enter path: ")
+        print("\n")
+    try:
+        with open(path) as f:
+            data = json.load(f)
+        tabs = tabs + list(data)
+    except:
+        print("no such path, choose choice another time")
+
+
+# the following functions are helper functions
+
+
+# used to get a title as input from user
 def add_title(tabs):
     if (len(tabs) > 0):
         keys = []
@@ -23,6 +115,8 @@ def add_title(tabs):
     return title
 
 
+
+# used to validate if the url provided by user is correct
 def validate_url():
     user_url = input("please enter url: ")
     while (not validators.url(user_url)):
@@ -31,6 +125,7 @@ def validate_url():
     return user_url
 
 
+# gets index from the user
 def get_index(tabs):
     index = 'not numeric'
     stop = False
@@ -48,12 +143,7 @@ def get_index(tabs):
         return index
 
 
-def add_tab(tabs):
-    user_url = validate_url()
-    title = add_title(tabs)
-    tabs.append({title:user_url, 'nested_tabs':[]})
-
-
+# given a url it returns the HTML content
 def get_html(url):
     headers = {
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
@@ -88,85 +178,8 @@ def print_menu():
     9. Exit
     ''')
 
-
-def add_nested_tab(tabs):
-    if len(tabs) == 0:
-        print("no opened tabs")
-    else:
-        index = -2
-        while (index >= len(tabs) or index < -1):
-            index = get_index(tabs)
-        title = add_title(tabs[index]['nested_tabs'])
-        print("\n")
-        url = validate_url()
-        tabs[index]['nested_tabs'].append({title:url})
-
-
-def remove_tab(tabs):
-    if len(tabs) > 0:
-        index = -2
-        while (index >= len(tabs) or index < -1):
-            index = get_index(tabs)
-        tabs.pop(index)
-    else:
-        print("nothing to remove, no tabs opened\n")
-
-
-def display(tabs):
-    if len(tabs) == 0:
-        print("nothing to display\n")
-    else:
-        index = -2
-        while (index >= len(tabs) or index < -1):
-            index = get_index(tabs)
-        dct = tabs[index]
-        print(get_html(dct[first_key(dct)]))
-        if len(dct['nested_tabs']) > 0:
-            for n_dct in dct['nested_tabs']:
-                print(get_html(n_dct[first_key(n_dct)]))
-
-
+# given that our dictionaries contain only 1 key, this function returns the key
 def first_key(dct):
     keys = dct.keys()
     keys = list(keys)
     return keys[0]
-
-
-def clear(tabs):
-    tabs = []
-
-
-def save_tabs(tabs):
-    path = ''
-    while ('json' not in path):
-        path = input("enter path: ")
-        print("\n")
-    with open(path, "w", encoding="utf-8") as json_file:
-        json.dump(tabs, json_file, ensure_ascii=False, indent=4)
-
-
-def load_tabs(tabs):
-    path = ''
-    while ('.json' not in path):
-        path = input("enter path: ")
-        print("\n")
-    try:
-        with open(path) as f:
-            data = json.load(f)
-        tabs = tabs + list(data)
-    except:
-        print("no such path, choose choice another time")
-
-
-def display_titles(tabs):
-    if len(tabs) == 0:
-        print("no opened tabs")
-    else:
-        for index in range(len(tabs)):
-            dct = tabs[index]
-            print(first_key(dct))
-            if len(dct['nested_tabs']) > 0:
-                for n_dct in dct['nested_tabs']:
-                    print(f'\t--{first_key(n_dct)}')
-
-
